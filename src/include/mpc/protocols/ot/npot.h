019a884da0397e3ea2dbb6023c7da31aa7a3760c
kiarash <kiarash.sedghi99@gmail.com>
2025-01-11 23:53:30 -0500

    Updating NPOT implementation

--- src/include/mpc/protocols/ot/npot.h
+++ src/include/mpc/protocols/ot/npot.h
@@ -6,116 +6,177 @@
 #include "math/bigint.h"
 
 namespace qst::mpc::protocols {
-
-
     template<typename IO_T>
-    class NPOT : public OT<IO_T> {
+    /**
+    * @brief Naor Pinkas OT class.
+    */
+    class NPOT final : public OT<IO_T> {
     public:
-        IO_T *io;
-        math::algstruct::Group *G;
-        bool delete_G;
-        int m_length;
-
-
-        void send_pt(IO_T* io, math::algstruct::Point* C) {
+        //todo
+        static void send_pt(IO_T *io, math::algstruct::Point *C) {
             size_t len = C->size();
             C->group->resize_scratch(len);
-            unsigned char * tmp = C->group->scratch;
-            io->send_data_internal(&len, 4);
+            unsigned char *tmp = C->group->scratch;
+            io->write(&len, 4);
             C->to_bin(tmp, len);
-            io->send_data_internal(tmp, len);
+            io->write(tmp, len);
         }
 
-        void recv_pt(IO_T* io, math::algstruct::Group * g, math::algstruct::Point* C) {
+        //todo
+        static void recv_pt(IO_T *io, math::algstruct::Group *g, math::algstruct::Point *C) {
             size_t len = 0;
-            io->recv_data_internal(&len, 4);
+            io->read(&len, 4);
             // assert(len <= 2048);
             g->resize_scratch(len);
-            unsigned char * tmp = g->scratch;
-            io->recv_data_internal(tmp, len);
+            unsigned char *tmp = g->scratch;
+            io->read(tmp, len);
             C->from_bin(g, tmp, len);
         }
 
 
+        NPOT(const IO_T &io, math::algstruct::Group *g, const std::uint64_t data_size)
+            : m_io(const_cast<IO_T *>(&io)), m_group(g ? g : new math::algstruct::Group()), m_data_size(data_size) {
+        }
 
 
+        /**
+         * @brief Send function for 1-2 OT instance of Naor Pinkas OT
+         * @param data1 First data
+         * @param data2 Second data
+         */
+        void send(const types::Data &data1, const types::Data &data2) override {
+            math::numbers::BigInt d{};
+            m_group->get_rand_bn(d);
+            math::algstruct::Point C = m_group->mul_gen(d);
+
+            send_pt(m_io, &C);
+            m_io->flush();
+
+            math::numbers::BigInt r{};
+            math::numbers::BigInt rc{};
+            math::algstruct::Point pk0{}, gr{}, Cr{};
+
+            m_group->get_rand_bn(r);
+            gr = m_group->mul_gen(r);
+            rc = r.mul(d, m_group->bn_ctx);
+            rc = rc.mod(m_group->order, m_group->bn_ctx);
+            Cr = m_group->mul_gen(rc);
+
+            recv_pt(m_io, m_group, &pk0);
+            send_pt(m_io, &gr);
+            m_io->flush();
+
+            types::Data masked_data[2];
+            pk0 = pk0.mul(r);
+            math::algstruct::Point inv = pk0.inv();
+            math::algstruct::Point pk1 = Cr.add(inv);
+
+            masked_data[0] = hash_point_to_data(pk0) ^ data1;
+            masked_data[1] = hash_point_to_data(pk1) ^ data2;
+
+            /* Convert Data to array of bytes */
+            auto byte_buffer = std::make_unique<char[]>(2 * m_data_size);
+            masked_data[0].to_bytes(byte_buffer.get());
+            masked_data[1].to_bytes(byte_buffer.get() + m_data_size);
+            m_io->write(byte_buffer.get(), 2 * m_data_size);
+        }
 
 
+        /**
+         * @brief Receive function for 1-2 OT instance of Naor Pinkas OT
+         * @param choice Choice bit
+         * @return Retrieved data
+         */
+        types::Data recv(const bool choice) override {
+            math::numbers::BigInt k{};
+            math::algstruct::Point gr{};
+            math::algstruct::Point pk[2];
 
+            math::algstruct::Point C;
+            m_group->get_rand_bn(k);
 
+            recv_pt(m_io, m_group, &C);
+            m_io->flush();
 
-        NPOT(const int length, const IO_T &io, math::algstruct::Group *g)
-        : io(const_cast<IO_T *>(&io)), G(g ? g : new math::algstruct::Group()), delete_G(g == nullptr), m_length {length} {}
+            if (choice) {
+                pk[1] = m_group->mul_gen(k);
+                math::algstruct::Point inv = pk[1].inv();
+                pk[0] = C.add(inv);
+            } else
+                pk[0] = m_group->mul_gen(k);
 
+            send_pt(m_io, &pk[0]);
+            recv_pt(m_io, m_group, &gr);
 
-        void send_onetwo(const types::Data &data1, const types::Data &data2) {
+            gr = gr.mul(k);
+
+            auto byte_buffer = std::make_unique<char[]>(2 * m_data_size);
+            m_io->read(byte_buffer.get(), 2 * m_data_size);
+
+            types::Data masked_data[2]{};
+            for (int i = 0; i < m_data_size; i++) {
+                masked_data[0].m_bytes.push_back(byte_buffer[i]);
+                masked_data[1].m_bytes.push_back(byte_buffer[i + m_data_size]);
+            }
+
+            const int ind{choice ? 1 : 0};
+            return masked_data[ind] ^ hash_point_to_data(gr);
+        }
+
+        /**
+         * @brief Send function for 1-N OT instance of Naor Pinkas OT
+         * @param arr_data1 List of first element of the tuples
+         * @param arr_data2 List of second element of the tuples
+         * @param count Number of tuples
+         */
+        void send_n(const types::Data *arr_data1, const types::Data *arr_data2, int count) override {
             math::numbers::BigInt d;
-            G->get_rand_bn(d);
-            math::algstruct::Point C = G->mul_gen(d);
+            m_group->get_rand_bn(d);
+            math::algstruct::Point C = m_group->mul_gen(d);
 
+            send_pt(m_io, &C);
 
-            // io->send_pt(&C);
-            // -----------------------------------
-            send_pt(io, &C);
-            std::cout << "S: SEND POINT\n";
+            m_io->flush();
 
-            io->flush();
-            m_length = 1;
+            auto *r = new math::numbers::BigInt[count];
+            auto *rc = new math::numbers::BigInt[count];
+            auto *pk0 = new math::algstruct::Point[count],
+                    *gr = new math::algstruct::Point[count],
+                    *Cr = new math::algstruct::Point[count];
 
-            auto * r = new math::numbers::BigInt[m_length];
-            auto * rc = new math::numbers::BigInt[m_length];
-            auto * pk0 = new math::algstruct::Point[m_length],
-                    *gr = new math::algstruct::Point[m_length],
-                    *Cr = new math::algstruct::Point[m_length];
-            for(int64_t i = 0; i < m_length; ++i) {
-                G->get_rand_bn(r[i]);
-                gr[i] = G->mul_gen(r[i]);
-                rc[i] = r[i].mul(d, G->bn_ctx);
-                rc[i] = rc[i].mod(G->order, G->bn_ctx);
-                Cr[i] = G->mul_gen(rc[i]);
+            for (int64_t i = 0; i < count; ++i) {
+                m_group->get_rand_bn(r[i]);
+                gr[i] = m_group->mul_gen(r[i]);
+                rc[i] = r[i].mul(d, m_group->bn_ctx);
+                rc[i] = rc[i].mod(m_group->order, m_group->bn_ctx);
+                Cr[i] = m_group->mul_gen(rc[i]);
             }
 
-            for(int64_t i = 0; i < m_length; ++i) {
+            for (int64_t i = 0; i < count; ++i)
+                recv_pt(m_io, m_group, &pk0[i]);
 
 
-                // io->recv_pt(G, &pk0[i]);
-                // --------------------------------
-                recv_pt(io, G, &pk0[i]);
-                std::cout << "S: RECV POINT\n";
-
-            }
-            for(int64_t i = 0; i < m_length; ++i) {
-                // io->send_pt(&gr[i]);
-                // ---------------------------------------------------
-                send_pt(io, &gr[i]);
-                std::cout << "S: SEND POINT\n";
+            for (int64_t i = 0; i < count; ++i)
+                send_pt(m_io, &gr[i]);
 
 
+            m_io->flush();
 
-            }
-
-            io->flush();
-
-            types::Data m[2];
-            for(int64_t i = 0 ; i < m_length; ++i) {
+            for (int64_t i = 0; i < count; ++i) {
                 pk0[i] = pk0[i].mul(r[i]);
                 math::algstruct::Point inv = pk0[i].inv();
                 math::algstruct::Point pk1 = Cr[i].add(inv);
 
-                std::cout << qst::math::algstruct::hash_point_to_data(pk0[i]).get_size() << data2.get_size() << "here\n";
-                m[0] = qst::math::algstruct::hash_point_to_data(pk0[i]) ^ data1;
-                m[0] = qst::math::algstruct::hash_point_to_data(pk1) ^ data2;
 
-                // m[0] = Hash::KDF(pk0[i]) ^ data1[i];
-                // m[1] = Hash::KDF(pk1) ^ data2[i];
-                char buff[100];
-                // io->send_data_internal(m, 2*(sizeof(qst::types::Data)));
+                types::Data masked_data[2]{};
+                masked_data[0] = hash_point_to_data(pk0[i]) ^ arr_data1[i];
+                masked_data[1] = hash_point_to_data(pk1) ^ arr_data2[i];
 
-                io->send_data_internal(buff, 100);
-                // send_pt(io, &gr[i]);
-
-                std::cout << "S: SEND DATA\n";
-
+                /* Convert Data to array of bytes */
+                auto byte_buffer = std::make_unique<char[]>(2 * m_data_size);
+                masked_data[0].to_bytes(byte_buffer.get());
+                masked_data[1].to_bytes(byte_buffer.get() + m_data_size);
+                m_io->write(byte_buffer.get(), 2 * m_data_size);
             }
 
             delete[] r;
@@ -123,206 +184,77 @@ namespace qst::mpc::protocols {
             delete[] Cr;
             delete[] rc;
             delete[] pk0;
-            std::cout << "send\n";
         }
 
-        types::Data recv_onetwo(bool choice) {
-
-            m_length = 1;
-            auto * k = new math::numbers::BigInt[m_length];
-            auto * gr = new math::algstruct::Point[m_length];
+        /**
+         * @brief Receive function for 1-N OT instance of Naor Pinkas OT
+         * @param arr_data Pointer to the list of data to be written into
+         * @param choices List of choice bits
+         * @param count Number of tuples
+         */
+        void recv_n(types::Data *arr_data, const bool *choices, const int count) override {
+            auto *k = new math::numbers::BigInt[count];
+            auto *gr = new math::algstruct::Point[count];
             math::algstruct::Point pk[2];
-            types::Data m[2];
             math::algstruct::Point C;
-            for(int64_t i = 0; i < m_length; ++i)
-                G->get_rand_bn(k[i]);
 
-            // io->recv_pt(G, &C);
-            // --------------------------------------
-            recv_pt(io, G, &C);
-            std::cout << "R: RECV POINT\n";
+            for (int64_t i = 0; i < count; ++i)
+                m_group->get_rand_bn(k[i]);
+            recv_pt(m_io, m_group, &C);
 
+            m_io->flush();
 
-
-
-
-
-
-
-
-            io->flush();
-
-            for(int64_t i = 0; i< m_length; ++i) {
-                if(choice) {
-                    pk[1] = G->mul_gen(k[i]);
+            for (int64_t i = 0; i < count; ++i) {
+                if (choices[i]) {
+                    pk[1] = m_group->mul_gen(k[i]);
                     math::algstruct::Point inv = pk[1].inv();
                     pk[0] = C.add(inv);
                 } else {
-                    pk[0] = G->mul_gen(k[i]);
+                    pk[0] = m_group->mul_gen(k[i]);
                 }
-                // io->send_pt(&pk[0]);
-                // ------------------------
-                send_pt(io, &pk[0]);
-                std::cout << "R: SEND POINT\n";
-                //
-                // size_t len = pk[0].size();
-                // pk[0].group->resize_scratch(len);
-                // unsigned char * tmp = pk[0].group->scratch;
-                // io->send_data_internal(&len, 4);
-                // pk[0].to_bin(tmp, len);
-                // io->send_data_internal(tmp, len);
-
-
-
-
-
-
-
-
-
-
-
-
-
+                send_pt(m_io, &pk[0]);
             }
 
-            for(int64_t i = 0; i < m_length; ++i) {
-                // io->recv_pt(G, &gr[i]);
-                recv_pt(io, G, &gr[i]);
-                std::cout << "R: RECV POINT\n";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            for (int64_t i = 0; i < count; ++i) {
+                recv_pt(m_io, m_group, &gr[i]);
                 gr[i] = gr[i].mul(k[i]);
             }
-            // io->flush();
+            m_io->flush();
 
-            for(int64_t i = 0; i < 1; ++i) {
-                int ind = choice ? 1 : 0;
+            for (int64_t i = 0; i < count; ++i) {
+                types::Data masked_data[2]{};
 
-                std::cout << ".................... Data:\n";
-                // io->recv_data_internal(m, 2*sizeof(qst::types::Data)); // todo
+                const int ind{choices[i] ? 1 : 0};
 
-                char buff[100];
-                io->recv_data_internal(buff, 100);
-                std::cout << "R: RECV data 50\n";
-                // data[i] = m[ind] ^ Hash::KDF(gr[i]);
-                // data
-                std::cout << "recv points - - - - - - - - - - \n";
-                return (m[ind] ^ math::algstruct::hash_point_to_data(gr[i]));
+                auto byte_buffer = std::make_unique<char[]>(2 * m_data_size);
+                m_io->read(byte_buffer.get(), 2 * m_data_size);
+
+                for (int i = 0; i < m_data_size; i++) {
+                    masked_data[0].m_bytes.push_back(byte_buffer[i]);
+                    masked_data[1].m_bytes.push_back(byte_buffer[i + 16]);
+                }
+
+                arr_data[i] = masked_data[ind] ^ hash_point_to_data(gr[i]);
             }
             delete[] k;
             delete[] gr;
-
         }
 
+    private:
+        /**
+         * Pointer to the base IO for the OT
+         */
+        IO_T *m_io;
 
+        /**
+         * @brief Underlying algebraic group structure
+         */
+        math::algstruct::Group *m_group;
 
-
-        void send(const types::Data &data1, const types::Data &data2) override {
-            // math::numbers::BigInt d;
-            // G->get_rand_bn(d);
-            // math::algstruct::Point C = G->mul_gen(d);
-            //
-            // io->send_pt(&C);
-            // io->flush();
-            //
-            // auto * r = new math::numbers::BigInt[m_length];
-            // auto * rc = new math::numbers::BigInt[m_length];
-            // auto * pk0 = new math::algstruct::Point[m_length],
-            //         *gr = new math::algstruct::Point[m_length],
-            //         *Cr = new math::algstruct::Point[m_length];
-            // for(int64_t i = 0; i < m_length; ++i) {
-            //     G->get_rand_bn(r[i]);
-            //     gr[i] = G->mul_gen(r[i]);
-            //     rc[i] = r[i].mul(d, G->bn_ctx);
-            //     rc[i] = rc[i].mod(G->order, G->bn_ctx);
-            //     Cr[i] = G->mul_gen(rc[i]);
-            // }
-            //
-            // for(int64_t i = 0; i < m_length; ++i) {
-            //     std::cout << "recv points";
-            //     io->recv_pt(G, &pk0[i]);
-            // }
-            // for(int64_t i = 0; i < m_length; ++i) {
-            //     std::cout << "sending points";
-            //     io->send_pt(&gr[i]);
-            // }
-            //
-            // io->flush();
-            //
-            // types::Data m[2];
-            // for(int64_t i = 0 ; i < m_length; ++i) {
-            //     pk0[i] = pk0[i].mul(r[i]);
-            //     math::algstruct::Point inv = pk0[i].inv();
-            //     math::algstruct::Point pk1 = Cr[i].add(inv);
-            //     m[0] = qst::math::algstruct::hash_point_to_data(pk0[i]) ^ data1[i];
-            //     m[0] = qst::math::algstruct::hash_point_to_data(pk1) ^ data2[i];
-            //
-            //     // m[0] = Hash::KDF(pk0[i]) ^ data1[i];
-            //     // m[1] = Hash::KDF(pk1) ^ data2[i];
-            //     // io->send_data(m, 2*sizeof(block));
-            // }
-            //
-            // delete[] r;
-            // delete[] gr;
-            // delete[] Cr;
-            // delete[] rc;
-            // delete[] pk0;
-            std::cout << "send";
-        }
-
-        types::Data recv(bool choice) override {
-            // auto * k = new math::numbers::BigInt[m_length];
-            // auto * gr = new math::algstruct::Point[m_length];
-            // math::algstruct::Point pk[2];
-            // types::Data m[2];
-            // math::algstruct::Point C;
-            // for(int64_t i = 0; i < m_length; ++i)
-            //     G->get_rand_bn(k[i]);
-            //
-            // io->recv_pt(G, &C);
-            // io->flush();
-            //
-            // // for(int64_t i = 0; i< m_length; ++i) {
-            // //     if(b[i]) {
-            // //         pk[1] = G->mul_gen(k[i]);
-            // //         math::algstruct::Point inv = pk[1].inv();
-            // //         pk[0] = C.add(inv);
-            // //     } else {
-            // //         pk[0] = G->mul_gen(k[i]);
-            // //     }
-            // //     io->send_pt(&pk[0]);
-            // // }
-            //
-            // for(int64_t i = 0; i < m_length; ++i) {
-            //     io->recv_pt(G, &gr[i]);
-            //     gr[i] = gr[i].mul(k[i]);
-            // }
-            // io->flush();
-            // for(int64_t i = 0; i < m_length; ++i) {
-            //     int ind = b[i] ? 1 : 0;
-            //     io->recv_data(m, 2*sizeof(block));
-            //     // data[i] = m[ind] ^ Hash::KDF(gr[i]);
-            //     // data
-            //     return (m[ind] ^ math::algstruct::hash_point_to_data(gr[i]));
-            // }
-            // delete[] k;
-            // delete[] gr;
-            return types::Data {};
-        }
+        /**
+         * @brief Size of each data to be sent in the OT in Bytes
+         */
+        std::uint64_t m_data_size;
     };
 }
 
